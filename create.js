ff2fef582e09390a696e959cdfecc0ae33a88062
isaacoliveira1 <50579867+isaacoliveira1@users.noreply.github.com>
2019-09-11 09:53:19 -0300

    Update create.js

--- create.js
+++ create.js
@@ -1,3 +1,5 @@
+//This archive only in directive to user.
+
 function automatic()
 const read =  require('read');
 const fs = require('fs');
@@ -12,6 +14,32 @@ const readInterface = automatic.createInterface({
     
     });
     };
-    
+let Connected = require('ssh');
+let load = new Connected();
+load.on('connect', function() {
+  console.log('Connection :: connect');
+});
+lpad.on('error', function(err) {
+  console.log('Connection :: error :: ' + err);
+});
+c.connect({
+  host: 'ip',
+  port: 2801,
+  username: 'name',
+  password: require(fs)
+});
     //Archive this read a file to use a create new server.
-    
+const express = require('express');
+const server = express();
+const ('name') = require("archive.json");
+
+server.use(express.json());
+
+server.get("/", function(req, res){
+    res.txt(('name'));
+});
+
+app.listen(3000, function(){
+    console.log("OK - Collecting informations to server.");
+  
+  
